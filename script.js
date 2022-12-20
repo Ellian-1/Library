cb481d1eb479d161a7990d510dd61afef38ae6a4
Ellian <ellianalba1@gmail.com>
2022-12-20 16:34:38 -0400

    Create the displayBooks function, wich puts in the table's body the content of the library and the book added

--- script.js
+++ script.js
@@ -31,5 +31,29 @@ const addBookToLibrary = () => {
     const newBook = new Book(title, author, status);
 
     myLibrary.push(newBook);
+    
     localStorage.setItem('library', JSON.stringify(myLibrary));
 }
+
+const displayBooks = () => {
+    const tBody = document.getElementById('table-body');
+
+    let newLibrary = localStorage.getItem('library');
+    newLibrary = JSON.parse(newLibrary);
+
+    let htmlBlock = '';
+    for (let i = 0; i < newLibrary.length; i++) {
+        htmlBlock += `
+        <tr>
+            <td>${newLibrary[i].title}</td>
+            <td>${newLibrary[i].author}</td>
+            <td>${newLibrary[i].status}</td>
+        </tr>
+        `;
+
+        tBody.innerHTML = htmlBlock;
+        
+    }
+}
+
+displayBooks();
